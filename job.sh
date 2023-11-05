ad8c4f979b6b0d13c04c62c91151828262fb70a0
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-11-05 01:55:54 +0100

    added job script for creating index file

---
+++ job.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+### -- set the job Name -- 
+#BSUB -J CREATE_INDEX
+
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+
+#BSUB -o logs/crete_index_%J.out
+#BSUB -e logs/crete_index_%J.err
+# -- end of LSF options --
+
+### -- specify queue -- 
+#BSUB -q gpua100
+
+### -- ask for number of cores -- 
+#BSUB -n 1
+
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- specify that we need X GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=5GB]"
+
+### -- set walltime limit: hh:mm --
+#BSUB -W 24:00
+
+### -- set the email address --
+#BSUB -u s204163@student.dtu.dk
+### -- send notification at start --
+##BSUB -B
+### -- send notification at completion--
+##BSUB -N
+
+source .env/bin/activate
+cd utils
+python3 create_index.py
