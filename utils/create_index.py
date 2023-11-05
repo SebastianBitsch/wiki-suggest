c854ab6c7326efcdf96ea20a4b2f3bafb2bb4343
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-11-05 20:15:47 +0100

    updated function to create index file

--- utils/create_index.py
+++ utils/create_index.py
@@ -1,30 +1,38 @@
-"""
-Create an index file of the .bz2 file that logs the byte location of every N rows
-Takes around 3 hours to run on a single cpu on hpc. 
-Is a one time operation that allows to faster look revisions and grab random ones, so it is worth the upfront cost
-"""
 import bz2
 
-ROWS_PER_REVISION = 14      # Should never change
-INDEX_RESOLUTION = 1000     # How many rows should there be between indices
-INDEX_ROWS_SPACING = ROWS_PER_REVISION * INDEX_RESOLUTION
+ROWS_PER_REVISION = 14                                          # The number of rows in a single revision object, should never change
+REVISIONS_PER_INDEX = 1000                                      # How many revisions should there be between indexing
+INDEX_ROWS_SPACING = ROWS_PER_REVISION * REVISIONS_PER_INDEX    # How many rows of the .bz2 file should pass between indexing
 
-file_path = '/work3/s204163/enwiki-20080103.main.bz2'
+def create_index_file(file_path: str) -> dict:
+    """
+    Create an index file that allow for faster look up in the gzip2 file by logging the
+    byte location at every N rows.
+    An index file is just the byte location of every N row in the file
+    Is a one time operation that allows to faster look revisions and grab random ones, so it is worth the upfront cost
+    Takes around ~3 hours to run on a single cpu on hpc.
+    """
 
-index = {}
+    index = {}
 
-# Crete the index
-with bz2.open(file_path, 'rb') as file:
-    byte_position = file.tell()
-    for i, line in enumerate(file):
-        if i % INDEX_ROWS_SPACING == 0:        
-            # Add the true row number, revision number, and byte position to the index
-            index[i // ROWS_PER_REVISION] = byte_position
-            print(f"Read {i}/~3500M lines")
+    # Crete the index
+    with bz2.open(file_path, 'rb') as file:
+        byte_position = file.tell()
+        for i, _ in enumerate(file):
+            if i % INDEX_ROWS_SPACING == 0:        
+                # Add the true row number, revision number, and byte position to the index
+                index[i // ROWS_PER_REVISION] = byte_position
+                print(f"Read {i}/1632271984 lines")
 
-        byte_position = file.tell() # Get the position of the row in bytes PRIOR to reading it        
+            byte_position = file.tell() # Get the position of the row in bytes PRIOR to reading it        
 
-# Save the index
-with open('index_file', 'w') as f:
-    for line_num, byte_pos in index.items():
-        f.write(f"{line_num}, {byte_pos}\n")
+    return index
+
+if __name__ == "__main__":
+    file_path = '/work3/s204163/wiki/wiki-revisions-dataset.bz2'
+    index = create_index_file()
+
+    # Save the index
+    with open('index_file', 'w') as f:
+        for line_num, byte_pos in index.items():
+            f.write(f"{line_num}, {byte_pos}\n")
