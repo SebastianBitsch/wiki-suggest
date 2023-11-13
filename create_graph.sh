b7d9e3e474d3b1c7949b4b73ae855685942faf28
SebastianBitsch <sebastianbitsch@protonmail.com>
2023-11-13 14:43:57 +0100

    changed log dir

--- create_graph.sh
+++ create_graph.sh
@@ -6,8 +6,8 @@
 ### -- Specify the output and error file. %J is the job-id --
 ### -- -o and -e mean append, -oo and -eo mean overwrite --
 
-#BSUB -o logs/create_graph_%J.out
-#BSUB -e logs/create_graph_%J.err
+#BSUB -o /work3/s204163/logs/create_graph_%J.out
+#BSUB -e /work3/s204163/logs/create_graph_%J.err
 # -- end of LSF options --
 
 ### -- specify queue -- 
@@ -20,7 +20,7 @@
 #BSUB -R "span[hosts=1]"
 
 ### -- specify that we need X GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=64GB]"
+#BSUB -R "rusage[mem=16GB]"
 
 ### -- set walltime limit: hh:mm --
 #BSUB -W 24:00
